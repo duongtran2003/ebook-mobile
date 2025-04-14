be88f63df6a1fef7d4932d4c26dabbd9f98f159d
duongtran2003 <dt005250@gmail.com>
2025-04-14 22:20:28 +0700

    updated navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,95 +1,3 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-
-// import HomeScreen from 'src/screens/HomeScreen';
-// // import FavoritesScreen from 'src/screens/FavoritesScreen';
-// import HistoryScreen from 'src/screens/HistoryScreen';
-// import LogoutScreen from 'src/screens/LogoutScreen';
-// import RankingView from 'src/screens/RankingView';
-
-// const Drawer = createDrawerNavigator();
-
-// export default function AppNavigator() {
-//   return (
-//     <NavigationContainer>
-//       <Drawer.Navigator initialRouteName="Trang chủ">
-//         <Drawer.Screen name="Trang chủ" component={HomeScreen} />
-//         <Drawer.Screen name="Bảng xếp hạng" component={RankingView} />
-//         {/* <Drawer.Screen name="Theo dõi" component={FavoritesScreen} /> */}
-//         <Drawer.Screen name="Lịch sử" component={HistoryScreen} />
-//         <Drawer.Screen name="Logout" component={LogoutScreen} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { View, Text, Image, StyleSheet } from 'react-native';
-
-// import HomeScreen from 'src/screens/HomeScreen';
-// // import FavoritesScreen from 'src/screens/FavoritesScreen';
-// import HistoryScreen from 'src/screens/HistoryScreen';
-// import LogoutScreen from 'src/screens/LogoutScreen';
-// import RankingView from 'src/screens/RankingView';
-
-// const Drawer = createDrawerNavigator();
-
-// // Component cho phần avatar + tên user
-// const HeaderRight = () => (
-//   <View style={styles.headerRightContainer}>
-//     <Text style={styles.username}>Tommy</Text>
-//     <Image
-//       source={require('../../assets/anh1.jpg')} // Đường dẫn ảnh avatar
-//       style={styles.avatar}
-//     />
-//   </View>
-// );
-
-// export default function AppNavigator() {
-//   return (
-//     <NavigationContainer>
-//       <Drawer.Navigator
-//         initialRouteName="Trang chủ"
-//         screenOptions={{
-//           headerTitle: '',
-//           headerRight: () => <HeaderRight />,
-//           headerRightContainerStyle: {
-//             paddingRight: 15,
-//           },
-//         }}
-//       >
-//         <Drawer.Screen name="Trang chủ" component={HomeScreen} />
-//         <Drawer.Screen name="Bảng xếp hạng" component={RankingView} />
-//         {/* <Drawer.Screen name="Theo dõi" component={FavoritesScreen} /> */}
-//         <Drawer.Screen name="Lịch sử" component={HistoryScreen} />
-//         <Drawer.Screen name="Logout" component={LogoutScreen} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   headerRightContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//   },
-//   username: {
-//     marginRight: 10,
-//     fontSize: 16,
-//     fontWeight: '500',
-//     color: '#333',
-//   },
-//   avatar: {
-//     width: 35,
-//     height: 35,
-//     borderRadius: 17.5,
-//   },
-// });
-
 import React, { useState } from 'react';
 import {
   NavigationContainer,
@@ -110,7 +18,7 @@ import HomeScreen from 'src/screens/HomeScreen';
 import HistoryScreen from 'src/screens/HistoryScreen';
 import LogoutScreen from 'src/screens/LogoutScreen';
 import RankingView from 'src/screens/RankingView';
-import ShowDetailBookScreen from 'src/screens/ShowDetailBookScreen';
+import HomeNavigator from './HomeNavigator';
 const Drawer = createDrawerNavigator();
 
 export default function AppNavigator() {
@@ -166,7 +74,7 @@ export default function AppNavigator() {
           },
         }}
       >
-        <Drawer.Screen name="Trang chủ" component={HomeScreen} />
+        <Drawer.Screen name="Trang chủ" component={HomeNavigator} />
         <Drawer.Screen name="Bảng xếp hạng" component={RankingView} />
         <Drawer.Screen name="Lịch sử" component={HistoryScreen} />
         <Drawer.Screen name="Logout" component={LogoutScreen} />
---
+++ src/navigation/HomeNavigator.tsx
@@ -0,0 +1,19 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from 'src/screens/HomeScreen'
+import ShowDetailBookScreen from 'src/screens/ShowDetailBookScreen'
+
+export type HomeStackParamList = {
+  Home: undefined
+  BookDetail: { bookId: string }
+}
+
+export default function HomeNavigator() {
+  const Stack = createStackNavigator<HomeStackParamList>()
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen options={{ title: 'Trang chủ' }} name="Home" component={HomeScreen} />
+      <Stack.Screen options={{ title: 'Chi tiết sách' }} name="BookDetail" component={ShowDetailBookScreen} />
+    </Stack.Navigator>
+  )
+}
